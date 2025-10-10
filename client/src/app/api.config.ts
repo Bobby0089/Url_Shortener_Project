3fec061b671588ffd6246132992e5bfd4cb9e32e
Bobby <learnings.bobby.0089@gmail.com>
2025-10-10 23:02:34 +0530

    remove client

--- client/src/app/api.config.ts
+++
@@ -1,4 +0,0 @@
-// export const API_BASE_URL = 'http://localhost:8080/urlapp/';
-
-
-export const API_BASE_URL =  'https://url-shortener-backend.osc-fr1.scalingo.io';
--- client/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://urlshortener-production.up.railway.app'
-};
