9b616acbceb7759871b5310f4bf9bb6314e0135b
Bobby Gupta <161326642+Bobby0089@users.noreply.github.com>
2025-10-10 19:57:02 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to server directory
+cd server
+
+# Install dependencies
+echo "Installing server dependencies..."
+npm install
+
+# Start the server
+echo "Starting server..."
+npm start
