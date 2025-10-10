90d099e1df630f81c60e42feb28adbe6506c568d
Bobby <learnings.bobby.0089@gmail.com>
2025-10-10 13:35:11 +0530

    deleting unnecessary files

--- src/main/java/com/aurionpro/repository/UserQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aurionpro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aurionpro.entity.UserQuery;
-
-public interface UserQueryRepository extends JpaRepository<UserQuery, Integer>{
-
-}
